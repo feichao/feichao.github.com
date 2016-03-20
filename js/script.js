b6056eacf2307d20dd0f6a93e423e9cfd6aa4423
FeichaoWan <len.may@foxmail.com>
2016-03-20 14:19:09 +0800

    update

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@
   // Caption
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
+      var $this = $(this);
+      var $parent = $this.parent();
+      if ($parent.hasClass('fancybox')) return;
+      if ($parent.hasClass('no-fancybox')) return;
 
       var alt = this.alt;
 
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+      if (alt) $this.after('<span class="caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+      $this.wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
     });
 
     $(this).find('.fancybox').each(function(){
